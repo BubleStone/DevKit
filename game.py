8cec0f83fa45071d506fe7b6dbe2e495da342a1b
UlerBrutal <136765243+UlerBrutal@users.noreply.github.com>
2023-06-17 01:34:50 +0300

    game
    text game test

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+def play_pot_thong():
+    print("Welcome to Pot Thong!")
+    print("I have chosen a number between 1 and 100.")
+    print("Try to guess the number in the fewest attempts possible.")
+
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low!")
+        elif guess > secret_number:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number in", attempts, "attempts.")
+            break
+
+    print("Thanks for playing Pot Thong!")
+
+play_pot_thong()
